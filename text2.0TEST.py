cc5a9aeb5618cc579a6281c61f381df73480d41d
joydunne <joydunne33@gmail.com>
2022-10-07 11:38:55 -0500

    test

--- text2.0TEST.py
+++ text2.0TEST.py
@@ -3,4 +3,5 @@ from textTest import *
 USFile = constText ("USconst.txt")
 
 print ("Number of Lines:", USFile.fileLength()) #() calls the function 
+
 print ("Number of Stanzas:", USFile.stanzaCount()) #() calls the function 

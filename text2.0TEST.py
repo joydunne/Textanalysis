163aba6b866d8b64f12a82dc3ead638a8282fbce
joydunne <joydunne33@gmail.com>
2022-10-07 11:36:59 -0500

    test

---
+++ text2.0TEST.py
@@ -0,0 +1,6 @@
+from textTest import *
+
+USFile = constText ("USconst.txt")
+
+print ("Number of Lines:", USFile.fileLength()) #() calls the function 
+print ("Number of Stanzas:", USFile.stanzaCount()) #() calls the function 
--- textTest.py
+++ textTest.py
@@ -1,14 +1,27 @@
-class text:
+class constText:
     '''Analyze texts using stats and such.'''
     def __init__ (self, filename):
-        self.fname = filename 
-        with open(self.fname) as f:
+        self.filename = filename 
+        with open(self.filename) as f:
             self.lines = f.readlines()
 
     def fileLength(self):
-        n = len(self.lines)
-        
+        n = 0 
+        for line in self.lines:
+            line = line.strip()
+            print (line, len(line))
+            if line != '':   #'' means an empty string 
+                n = n + 1 # n is called a counter 
         return (n) 
+
+    def stanzaCount(self): 
+        n = 0 
+        for line in self.lines:
+            line = line.strip()
+            if line == '':
+                n = n + 1
+            c = n + 1 
+        return c 
     
 
         

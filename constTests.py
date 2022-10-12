95da5ae779902f00c026e96a8c6adb3ed4a99935
joydunne <joydunne33@gmail.com>
2022-10-12 12:07:09 -0500

    updated functions

--- constTests.py
+++ constTests.py
@@ -4,13 +4,19 @@ USFile = constText ("USconst.txt")
 CanadaFile = constText ("canadaConst.txt")
 NetherlandFile = constText ("netherlandConst.txt")
 
+print ("Number of Lines:", USFile.fileLength()) #() calls the function 
 #print ("Number of Lines:", CanadaFile.fileLength()) #() calls the function 
+#print ("Number of Lines:", NetherlandFile.fileLength()) #() calls the function 
 
+print ("Number of Stanzas:", USFile.stanzaCount()) #() calls the function 
 #print ("Number of Stanzas:", CanadaFile.stanzaCount()) #() calls the function 
+#print ("Number of Stanzas:", NetherlandFile.stanzaCount()) #() calls the function 
 
-w = "faith"
-print (f"Number of {w}:", {CanadaFile.religiousWords(w)})
+w = "religion"
+print (f"Number of {w}:", USFile.religiousWords(w))
+#print (f"Number of {w}:", {CanadaFile.religiousWords(w)})
+#print (f"Number of {w}:", {NetherlandFile.religiousWords(w)})
 
-for s in CanadaFile.sentences:
-    print (s)
-print ("Number of Sentences:", len(CanadaFile.sentences))
+print ("Number of Sentences:", len(USFile.sentences))
+#print ("Number of Sentences:", len(CanadaFile.sentences))
+#print ("Number of Sentences:", len(NetherlandFile.sentences))
